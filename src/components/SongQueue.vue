0df93c0a7630e5bacc8eb57f5e84e34a693b1f33
Thomas Britton <britton.tm@gmail.com>
2017-01-17 22:37:44 -0600

    Added skeleton of queue component.

---
+++ src/components/SongQueue.vue
@@ -0,0 +1,21 @@
+<template>
+  <section id="song-queue">
+    <h2>Song Queue</h2>
+    <ol>
+      <li v-for="song in queue">
+        {{ song.title }}
+      </li>
+    </ol>    
+  </section>  
+</template>
+
+<script>
+export default {
+  name: 'song-queue',
+  data () {
+    return {
+      queue: this.$store.state.queue
+    }
+  }
+}
+</script>
